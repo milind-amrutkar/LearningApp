02a1d0f204a96c394fac3213824d57f59d3cc9c0
Milind Amrutkar <milind.amrutkar@priceline.com>
2023-10-13 18:21:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.demoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.demoapp.ui.theme.DemoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DemoAppTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Greeting("Android")
+                    SampleButton()
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun SampleButton() {
+    Button(
+        onClick = { },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp),
+        shape = RoundedCornerShape(20.dp),
+        border = BorderStroke(2.dp, Color.Blue),
+        colors = ButtonDefaults.buttonColors(
+            contentColor = Color.Gray,
+            containerColor = Color.White
+        )
+    ) {
+        Text(
+            text = stringResource(R.string.click_me),
+            fontSize = 14.sp,
+            modifier = Modifier.padding(horizontal = 30.dp, vertical = 6.dp)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DemoAppTheme {
+        Greeting("Android")
+    }
+}
